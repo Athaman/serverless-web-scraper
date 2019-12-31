1fa7ddfe67b3454b4fd404766367172c3725e02b
Keone Martin <keone@tapendium.com>
2020-01-01 00:00:02 +1100

    gotta polish some stuff but happy new year

--- handler.js
+++ handler.js
@@ -2,21 +2,34 @@
 const { getPage, parsePage, db } = require('./utils');
 
 module.exports.scrape = async event => {
+  const details = event; // avoid changing variable names on the flight for confusion sake
   //  fetch the page 
-  getPage(event)
+  getPage(details)
   //  parse the page   
   .then(page => parsePage(page))
   //  save ratings to db
-  .then(stars => db(stars));
-  // return {
-  //   statusCode: 200,
-  //   body: JSON.stringify(
-  //     {
-  //       message: 'Go Serverless v1.0! Your function executed successfully!',
-  //       input: event,
-  //     },
-  //     null,
-  //     2
-  //   ),
-  // };
+  .then(stars => db(stars, details))
+  
+  .then(()=>{
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Seems like that went well and was added to the db',
+          input: event,
+        },
+        null,
+        2
+      ),
+    };
+  })
+  .catch(error => {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Something went horribly wrong',
+        error: error
+      })
+    }
+  });
 };
--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,28 @@
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
 
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
-module.exports = (stars) => {
-    console.log(stars);
+module.exports = (stars, details) => {
+    const { user, repository } = details;
+    const date = JSON.stringify(new Date());
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: {
+            id: uuid.v1(),
+            user: user,
+            repository: repository,
+            stars: stars,
+            scrapedAt: date
+        }
+    };
+
+    dynamo.put(params, error => {
+        if(error) {
+            console.error(`Error returned from db on save ${JSON.stringify(error)}`);
+            return Promise.reject(`Error returned from db on save ${JSON.stringify(error)}`);
+        } else {
+            return Promise.resolve(params.Item);
+        }
+    });
 }
